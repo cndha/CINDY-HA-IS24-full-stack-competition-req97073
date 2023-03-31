2669ebc6362555f9718b301b95f3c13edc5ccae4
Cindy Ha <cindyhaa@gmail.com>
2023-03-31 04:46:00 -0700

    dummy data fill

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,6 @@ function App() {
           return i.productId
         })
         setIdList(idArray);
-        console.log(idArray);
       })
       .catch(err => console.log(err));
   }
--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const data = require('./data.json');
 app.use(express.json()); // converts all incoming put, patch, post to js objects
 
 app.get('/', (req, res) => {
-  res.json('Api is running succesfully...');
+  res.sendStatus(200);
 });
 
 app.get('/api', (req, res) => {
   res.json(data);
+  res.sendStatus(200);
 });
 
 app.get('/api/products', (req, res) => {
@@ -25,18 +26,17 @@ app.get('/api/products/:id', (req, res) => {
 });
 
 
-app.patch('/api/products/:id', (req, res) => {
-  res.json(req.body);
-
+app.patch('/api/products/:id', (req, res) => {  
   const db = data.products;
   const productIndex = db.findIndex((x) => x.productId === parseInt(req.params.id));
   db.splice(productIndex, 1, req.body);
+  res.json(req.body);
 })
 
 
 app.post('/api/products', (req, res) => {
-  res.json(req.body);
   data.products.push(req.body);
+  res.json(req.body);
 })
 
 
